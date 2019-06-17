a9eb69ee0933736ecc02d88ba47346698594d17a
LOPSANRU <rlopezsanz@ree.es>
2019-06-17 16:11:55 +0200

    Prueba Commit desde Pycharm

---
+++ hello.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        d
+# Purpose:      Pruebas Git
+#
+# Author:      Alumno47
+#
+# Created:     11/02/2019
+# Copyright:   (c) Alumno47 2019
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    listaRLS={"Soy un ":3, "hola ":5,"mundo ":7}
+    print(list(listaRLS.items()))
+
+if __name__ == '__main__':
+    main()
