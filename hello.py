22027adcd4e10157a8a0bf411d72c1fa9db9325c
RubenL <rlopezsanz@gmail.com>
2021-04-14 22:16:30 +0200

    desde VSCode windows personal

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@
 # Name:        d
 # Purpose:      Pruebas Git
 #
-# comentario nuevo + otro mas
+# comentario nuevo + otro mas + otroooo
 #
 # Created:     11/02/2019
 # Copyright:   (c) Alumno47 2019
@@ -14,5 +14,5 @@ def main():
     print(list(listaRLS.items()))
 
 if __name__ == '__main__':
-    print('hola chavalotes') #corrijo desde web github
+    print('hola chavalotes') #corrijo desde web github 
     main()
